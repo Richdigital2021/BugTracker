f46dca99b618fadca4b62952527b7fe01650c81b
Richdigital2021 <creativegraphicshub@gmail.com>
2025-07-14 12:46:43 +0100

    Updated the vie.config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,12 @@
-// client/vite.config.js
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  root: resolve(__dirname, "."), // 👈 or just remove this line completely
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: resolve(__dirname, "../dist"),
+    outDir: "dist", // Outputs to client/dist
     emptyOutDir: true,
   },
 });
