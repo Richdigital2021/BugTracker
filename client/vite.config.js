1f9cd352fa4474b3529c1318bf18231eecfb37de
Richdigital2021 <creativegraphicshub@gmail.com>
2025-07-14 12:30:10 +0100

    Made changes to the folder structure

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
-// vite.config.js
+// client/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  root: resolve(__dirname, "client"),
-  plugins: [react()],
+  root: resolve(__dirname, "."), // 👈 or just remove this line completely
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: resolve(__dirname, "dist"),
+    outDir: resolve(__dirname, "../dist"),
     emptyOutDir: true,
   },
 });
